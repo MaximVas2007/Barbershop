49c667e65281cafe1ca8a0340db1872b51872d9f
Unknown <unknown@example.com>
2020-03-14 15:09:40 +0300

    employee methods in Barbershop class added

--- Barbershop/Barbershop/Barbershop.cs
+++ Barbershop/Barbershop/Barbershop.cs
@@ -10,6 +10,17 @@ namespace Barbershop
     {
         public static List<Customer> customers = new List<Customer>();
         public static List<Service> services = new List<Service>();
+        public static List<Employee> employees = new List<Employee>();
+
+        public static int find_emloyee(Employee employee)
+        {
+            for(int i = 0; i < customers.Count; i++)
+            {
+                if (employees[i].is_compare(employee))
+                    return i;
+            }
+            return -1;
+        }
 
         public static int find_customer(Customer customer)
         {
@@ -29,6 +40,20 @@ namespace Barbershop
                     return i;
             }
             return -1;
+        }
+
+        public static bool add_emloyee(Employee employee)
+        {
+            int employee_index = find_emloyee(employee);
+            if (employee_index != -1)
+                return false;
+            employees.Add(employee);
+            return true;
+        }
+
+        public static bool add_emloyee (string name, string lastname, string position)
+        {
+            return add_emloyee(new Employee(name,lastname, position));
         }
 
         public static bool add_customer(Customer customer)
